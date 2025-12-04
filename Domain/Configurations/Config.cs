e7ef9797e17d92f19d1d1a08c6a6a8316b4c481f
Hany Abdou <Hany.Abdou@cubicsystems.com>
2025-12-04 15:28:37 +0200

    Add Config class and remove WeatherForecast components
    Introduced a `Config` class for centralized configuration management, leveraging `Microsoft.Extensions.Configuration` to load settings from `appsettings.json`. Added package references for `Microsoft.Extensions.Configuration.Abstractions` and `Microsoft.Extensions.Configuration.Json` in `Domain.csproj`.
    
    Removed the `WeatherForecastController` and `WeatherForecast` classes as they were no longer needed. Updated `Mentor.csproj` to include a `Controllers` folder.
    
    These changes improve maintainability by removing unused code and introducing a scalable configuration management system.

---
+++ Domain/Configurations/Config.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Enums;
+using Microsoft.Extensions.Configuration;
+
+namespace Domain.Configurations
+{
+    public static class Config
+    {
+        static Config()
+        {
+            IConfigurationBuilder builder =
+                new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            IConfiguration configuration = builder.Build();
+            UpdateProperties(Env);
+        }
+        public static SysEnvironment Env = SysEnvironment.Local;
+        private static void UpdateProperties(SysEnvironment env)
+        {
+            switch (env)
+            {
+                case SysEnvironment.Production:
+                    Env = SysEnvironment.Production;
+                    //
+                    break;
+                case SysEnvironment.Stg:
+                    Env = SysEnvironment.Stg;
+                    //
+                    break;
+                case SysEnvironment.Development:
+                    Env = SysEnvironment.Development;
+                    //
+                    break;
+                case SysEnvironment.Local:               
+                    Env = SysEnvironment.Local;
+                    //Write_DefaultConnection = "Server=localhost:5432;User Id=postgres;Password=123;Database=sela_local";
+                    //Read_DefaultConnection = "Server=localhost:5432;User Id=postgres;Password=123;Database=sela_local";
+                    break;
+            }
+        }
+    }
+}
